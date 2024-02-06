d9870e7fffe0619848d3379faf6683c1c469eb46
JellyYJ <lyj10000h@gmail.com>
2024-02-06 16:52:51 +0000

    update token for Map api

--- frontend/config.js
+++ frontend/config.js
@@ -3,6 +3,6 @@ const server = "http://localhost:3000";
 
 // public key for mapbox
 // with up to 50000 loads and 3 seats every month for free
-const mapboxPublicKey = "pk.eyJ1IjoiYmJlYXMiLCJhIjoiY2xmbG80OGc1MDNzeTNxbnAyN21neTFreiJ9.oquvrPZnCcP_3W-Z-EZd6g";
+const mapboxPublicKey = "pk.eyJ1IjoieWlqaWEwMSIsImEiOiJjbHNhbGV6dnAwM3VqMmtuemhmMGhndmRnIn0.5znqvxfRGEXcognmqmBP_A";
 
 export { server, mapboxPublicKey };
